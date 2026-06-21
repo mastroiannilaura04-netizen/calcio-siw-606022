493c108a7d49422bfd272e8926f2c6f42a4fd142
Laura Mastroianni <mastroiannilaura04@gmail.com>
2026-06-21 15:26:23 +0200

    changes arbitri

--- calcio/src/main/java/it/uniroma3/siw/calcio/controller/ArbitroController.java
+++ calcio/src/main/java/it/uniroma3/siw/calcio/controller/ArbitroController.java
@@ -1,5 +1,10 @@
 package it.uniroma3.siw.calcio.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +13,8 @@ import it.uniroma3.siw.calcio.model.Arbitro;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ArbitroController {
@@ -17,24 +24,68 @@ public class ArbitroController {
     public ArbitroController(ArbitroService arbitroService) {
         this.arbitroService = arbitroService;
     }
+
     @GetMapping("/arbitri")
     public String mostraArbitri(Model model) {
         model.addAttribute("arbitri", this.arbitroService.findAll());
         return "listArbitro";
     }
+
     @GetMapping("/arbitri/new")
     public String mostraFormNuovoArbitro(Model model) {
         model.addAttribute("arbitro", new Arbitro());
         return "formArbitro";
     }
+
+    @GetMapping("/arbitri/{id}")
+    public String mostraArbitro(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("arbitro", this.arbitroService.findById(id));
+        return "showArbitro";
+    }
+
     @GetMapping("/arbitri/{id}/edit")
     public String mostraFormModificaArbitro(@PathVariable("id") Long id, Model model) {
         model.addAttribute("arbitro", this.arbitroService.findById(id));
         return "formArbitro";
     }
+
     @PostMapping("/arbitri")
-    public String salvaArbitro(@ModelAttribute("arbitro") Arbitro arbitro) {
+    public String salvaArbitro(@ModelAttribute("arbitro") Arbitro arbitro,
+                               @RequestParam("fileFoto") MultipartFile fileFoto)
+            throws IOException {
+
+        if (!fileFoto.isEmpty()) {
+            String nomeFile = System.currentTimeMillis() + "_" + fileFoto.getOriginalFilename();
+            Path percorso = Paths.get("src/main/resources/static/images/arbitri/" + nomeFile);
+            Files.write(percorso, fileFoto.getBytes());
+            arbitro.setFoto("/images/arbitri/" + nomeFile);
+        }
+
         this.arbitroService.save(arbitro);
+
         return "redirect:/arbitri";
+    }
+
+    @PostMapping("/arbitri/{id}")
+    public String modificaArbitro(@PathVariable("id") Long id,
+                                  @ModelAttribute("arbitro") Arbitro arbitro,
+                                  @RequestParam("fileFoto") MultipartFile fileFoto)
+            throws IOException {
+
+        Arbitro vecchioArbitro = this.arbitroService.findById(id);
+
+        if (!fileFoto.isEmpty()) {
+            String nomeFile = System.currentTimeMillis() + "_" + fileFoto.getOriginalFilename();
+            Path percorso = Paths.get("src/main/resources/static/images/arbitri/" + nomeFile);
+            Files.write(percorso, fileFoto.getBytes());
+            arbitro.setFoto("/images/arbitri/" + nomeFile);
+        } else {
+            arbitro.setFoto(vecchioArbitro.getFoto());
+        }
+
+        arbitro.setId(id);
+        this.arbitroService.save(arbitro);
+
+        return "redirect:/arbitri/" + id;
     }
 }
--- calcio/src/main/java/it/uniroma3/siw/calcio/model/Arbitro.java
+++ calcio/src/main/java/it/uniroma3/siw/calcio/model/Arbitro.java
@@ -20,73 +20,56 @@ public class Arbitro {
     private String cognome;
     private String codiceArbitrale;
 
+    private String foto;
+
     @OneToMany(mappedBy = "arbitro")
-private List<Partita> partite = new ArrayList<>();
+    private List<Partita> partite = new ArrayList<>();
 
     public List<Partita> getPartite() {
         return partite;
     }
+
     public void setPartite(List<Partita> partite) {
         this.partite = partite;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getCognome() {
         return cognome;
     }
+
     public void setCognome(String cognome) {
         this.cognome = cognome;
     }
+
     public String getCodiceArbitrale() {
         return codiceArbitrale;
     }
+
     public void setCodiceArbitrale(String codiceArbitrale) {
         this.codiceArbitrale = codiceArbitrale;
     }
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-        result = prime * result + ((cognome == null) ? 0 : cognome.hashCode());
-        result = prime * result + ((codiceArbitrale == null) ? 0 : codiceArbitrale.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Arbitro other = (Arbitro) obj;
-        if (nome == null) {
-            if (other.nome != null)
-                return false;
-        } else if (!nome.equals(other.nome))
-            return false;
-        if (cognome == null) {
-            if (other.cognome != null)
-                return false;
-        } else if (!cognome.equals(other.cognome))
-            return false;
-        if (codiceArbitrale == null) {
-            if (other.codiceArbitrale != null)
-                return false;
-        } else if (!codiceArbitrale.equals(other.codiceArbitrale))
-            return false;
-        return true;
+
+    public String getFoto() {
+        return foto;
     }
 
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
